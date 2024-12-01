f18a053447140d27fb1c6315a65aa6d8a6f469a3
Fred-LHH <lhh156840790092023@163.com>
2024-12-01 16:36:39 +0800

    update Size.py

--- Factor/Size.py
+++ Factor/Size.py
@@ -1,18 +1,26 @@
 import pandas as pd
 import numpy as np
-import utils as ut
+from utils import Function
 
-def Size(data: pd.DataFrame):
-    '''
-    传入带有流通市值的DataFrame
-    columns: code date circ_mv
-    '''
-    data['circ_mv'] = data['circ_mv'] / 1e4 # 以亿元为单位
-    data['LNCAP'] = np.log(data['circ_mv']+1)
-    data['sub_MIDCAP'] = data['LNCAP'] ** 3
-    # 截面正交
-    MIDCAP = data.groupby('trade_date').apply(ut._regress, 'sub_MIDCAP', 'LNCAP', 'circ_mv')
-    MIDCAP.name = 'MIDCAP'
-    data = data.merge(MIDCAP.reset_index())
-    data['Size'] = (data['LNCAP'] + data['MIDCAP']) / 2
-    return data[['code', 'trade_date', 'LNCAP', 'MIDCAP', 'Size']]
+class Size(Function):
+
+    def __init__(self,
+                 df: pd.DataFrame):
+        '''
+        传入带有流通市值的DataFrame, columns: ['code', 'trade_date', 'circ_mv']
+        '''
+        df['circ_mv'] = df['circ_mv'] / 1e4 # 以亿元为单位
+        self.df = df
+        self.df['LNCAP'] = np.log(df['circ_mv']+1)
+        self.df = self.MIDCAP()
+        self.df['SIZE'] = (self.df['LNCAP'] + self.df['MIDCAP']) / 2
+    
+    def MIDCAP(self):
+        self.df['sub_MIDCAP'] = self.df['LNCAP'] ** 3
+        MIDCAP = self.df.groupby('trade_date').apply(self._regress, 'sub_MIDCAP', 'LNCAP', 'circ_mv')
+        MIDCAP.name = 'MIDCAP'
+        self.df = self.df.merge(MIDCAP.reset_index())
+        return self.df
+    
+    def get_single_factor(self, col):
+        return self._get_factor(self.df, col)
