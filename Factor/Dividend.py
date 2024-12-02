d332fe71632e6cbf510944e27ce7e5a13d7cf0d9
Fred-LHH <lhh156840790092023@163.com>
2024-12-02 23:31:06 +0800

    Dividend.py

---
+++ Factor/Dividend.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+from utils import Function
+
+class Dividend(Function):
+
+    def __init__(self,
+                 dividend: pd.DataFrame,
+                 price: pd.DataFrame):
+        
+        self.dividend = dividend[dividend['div_proc'] == '实施'][['code', 'end_date', 'ann_date', 'cash_div_tax']]
+        self.dividend.reset_index(drop=True, inplace=True)
+        self.price = price
+
+        self.DTOP = self.DTOP()
+
+    def DTOP(self):
+        ttm_dividend = self._convert_ttm(self.dividend, cols=['cash_div_tax'])
+
+        close = self.get_price_last_month_end(self.price)
+        ttm_dividend = self._pubDate_align_tradedate(ttm_dividend, 'ann_date', '20240831')
+        dividend = pd.merge(ttm_dividend, close, on=['code', 'trade_date'], how='left')
+        dividend['DTOP'] = dividend.eval('cash_div_tax_TTM / last_month_end_close')
+        DTOP = dividend[['code', 'trade_date', 'DTOP']]
+
+        return DTOP
+
+
+
+
+
+
+
+def cal_DTOP(dividend,
+              price):
+    dividend = dividend[dividend['div_proc'] == '实施']
+    dividend = dividend[['code', 'end_date', 'ann_date', 'cash_div_tax']]
+    dividend.reset_index(drop=True, inplace=True)
+    ttm_dividend = ut._convert_ttm(dividend, cols=['cash_div_tax'])
+
+    close = ut.get_price_last_month_end(price)
+    ttm_dividend = ut._pubDate_align_tradedate(ttm_dividend, 'ann_date', '20240831')
+    dividend = pd.merge(ttm_dividend, close, on=['code', 'trade_date'], how='left')
+    dividend['DTOP'] = dividend.eval('cash_div_tax_TTM / last_month_end_close')
+    DTOP = dividend[['code', 'trade_date', 'DTOP']]
+
+    return DTOP
+
+
+
+
